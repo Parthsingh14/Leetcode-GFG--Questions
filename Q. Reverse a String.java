093c0eb36681881869fd88a053750db12b2ad6d7
Parth Singh <71585861+Parthsingh14@users.noreply.github.com>
2024-03-18 10:09:48 +0530

    Parth GFG

---
+++ Q. Reverse a String.java
@@ -0,0 +1,16 @@
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        String ch="";
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            ch=ch+str.charAt(i);
+        }
+        return ch;
+        
+    }
+}
